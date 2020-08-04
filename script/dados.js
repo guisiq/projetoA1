670e1629ee39e2fd9f199e53419247d580c9649e
guisiq <63610173+guisiq@users.noreply.github.com>
2020-08-04 16:42:54 -0300

    comesando a andar

---
+++ script/dados.js
@@ -0,0 +1,26 @@
+let personagem = {
+    naturesa: {
+        especie: [],
+        ocupacao: [],
+        atributo: [],
+        asesorio: [],
+    },
+    comico: {
+        especie: [],
+        ocupacao: [],
+        atributo: [],
+        asesorio: [],
+    },
+    fantasia: {
+        especie: [],
+        ocupacao: [],
+        atributo: [],
+        asesorio: [],
+    },
+    medieval: {
+        especie: [],
+        ocupacao: [],
+        atributo: [],
+        asesorio: [],
+    }
+}
